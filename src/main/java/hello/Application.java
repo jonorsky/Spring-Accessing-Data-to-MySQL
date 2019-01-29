097dd268adc5247013c35fce11332a6a7c9d71f0
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2019-01-29 11:50:25 +0800

    Fix Build

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,11 @@
-package com.frostflux;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMysqlApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMysqlApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
-
